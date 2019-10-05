cc9b8f617bfe78a8aeea2b380793b330e3290541
Victor Turansky <victor.turansky@gmail.com>
2019-10-05 10:51:45 +0300

    Add root build script

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
+import org.jetbrains.kotlin.gradle.plugin.KotlinJsPluginWrapper
+
+plugins {
+    kotlin("js") version "1.3.50" apply false
+}
+
+allprojects {
+    repositories {
+        jcenter()
+    }
+}
+
+subprojects {
+    plugins.withType<KotlinJsPluginWrapper> {
+        tasks.withType<KotlinJsCompile>().configureEach {
+            kotlinOptions {
+                moduleKind = "commonjs"
+                allWarningsAsErrors = true
+            }
+        }
+    }
+}
+
+tasks.wrapper {
+    gradleVersion = "5.6.2"
+    distributionType = Wrapper.DistributionType.ALL
+}
