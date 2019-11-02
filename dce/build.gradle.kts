c4d6f255ed9e7a20883b03d14dfb6cd71a255dbb
Victor Turansky <victor.turansky@gmail.com>
2019-11-02 18:23:36 +0300

    Fixed DCE plugin version

--- dce/build.gradle.kts
+++ dce/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("js") version "1.3.50" apply false
+    id("kotlin-dce-js") version "1.3.50" apply false
+
     id("com.github.turansky.yfiles") version "0.1.0" apply false
 }
 
