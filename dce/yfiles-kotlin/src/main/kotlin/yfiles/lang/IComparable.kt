ffdc055a1f3313ac73131cca5a85f9bc9f2ee6c6
Victor Turansky <victor.turansky@gmail.com>
2019-11-02 15:56:17 +0300

    Fix IComparable

--- dce/yfiles-kotlin/src/main/kotlin/yfiles/lang/IComparable.kt
+++ dce/yfiles-kotlin/src/main/kotlin/yfiles/lang/IComparable.kt
@@ -1,7 +1,10 @@
 @file:JsModule("yfiles")
+@file:Suppress("NESTED_CLASS_IN_EXTERNAL_INTERFACE")
 
 package yfiles.lang
 
 external interface IComparable<T> {
     operator fun compareTo(other: T): Int
+
+    companion object
 }
