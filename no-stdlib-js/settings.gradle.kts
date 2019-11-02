e1ce9122183d281742388bd008d5199cc3253b83
Victor Turansky <victor.turansky@gmail.com>
2019-11-03 02:58:01 +0300

    Empty Gradle project

---
+++ no-stdlib-js/settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "no-stdlib-js"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        jcenter()
+        maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
+    }
+}
