447ec0e9efa33f0357b6de068e8be3900a226ee5
Victor Turansky <victor.turansky@gmail.com>
2019-11-02 15:53:25 +0300

    Add yFiles gradle plugin

--- dce/build.gradle.kts
+++ dce/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     kotlin("js") version "1.3.50" apply false
+    id("com.github.turansky.yfiles") version "0.1.0" apply false
 }
 
 allprojects {
     repositories {
+        gradlePluginPortal()
         jcenter()
     }
 }
