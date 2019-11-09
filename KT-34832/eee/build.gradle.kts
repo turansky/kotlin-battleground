93e542990b8972b6bd59833154673423a4939fb9
Victor Turansky <victor.turansky@gmail.com>
2019-11-09 03:39:53 +0300

    KT-34832. Add EEE class

--- KT-34832/eee/build.gradle.kts
+++ KT-34832/eee/build.gradle.kts
@@ -1,3 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("multiplatform")
+}
+
+kotlin {
+    jvm {
+        tasks.withType<KotlinCompile> {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+
+    js {
+        nodejs()
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib"))
+            }
+        }
+
+        val jsMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-js"))
+            }
+        }
+    }
 }
---
+++ KT-34832/eee/src/commonMain/kotlin/com/test/eee/EEE.kt
@@ -0,0 +1,6 @@
+package com.test.eee
+
+data class EEE(
+    val e1: Int,
+    val e2: Int
+)
