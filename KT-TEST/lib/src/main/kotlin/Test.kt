f688d5b6dbf5bea8db7f0cec5cedbe25160afee4
Victor Turansky <victor.turansky@gmail.com>
2019-12-14 20:03:30 +0300

    Add Singleton object

--- KT-TEST/lib/src/main/kotlin/Test.kt
+++ KT-TEST/lib/src/main/kotlin/Test.kt
@@ -1,3 +1,12 @@
-fun test() {
-    println("TEST")
+object Singleton {
+    private var firstCalled: Boolean = false
+
+    fun test(): String {
+        if (!firstCalled) {
+            firstCalled = true
+            return "first"
+        }
+
+        return "not first"
+    }
 }
