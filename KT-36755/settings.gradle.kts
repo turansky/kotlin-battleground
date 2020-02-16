e1caee5993e480d594decfa41fc9221b81be24f2
Victor Turansky <victor.turansky@gmail.com>
2020-02-17 00:12:20 +0300

    Add empty gradle project

---
+++ KT-36755/settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "kotlin-webpack"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
+    }
+}
