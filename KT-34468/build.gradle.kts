f0836283f38b36dc9ccab5d7e5ccb41b5533e5ce
Victor Turansky <victor.turansky@gmail.com>
2020-03-23 20:55:23 +0300

    KT-37587. NPM dependency for GitHub version test

--- KT-34468/build.gradle.kts
+++ KT-34468/build.gradle.kts
@@ -11,6 +11,7 @@ kotlin.target.browser()
 
 dependencies {
     implementation(kotlin("stdlib-js"))
+    implementation(npm("@yworks/optimizer", "1.0.6"))
 
     testImplementation(kotlin("test-js"))
 }
