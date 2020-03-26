02e272da23d4c9bf1f094f613b8751db9a263af5
Victor Turansky <victor.turansky@gmail.com>
2020-03-26 05:08:49 +0300

    KT-SAMS. Add build script

---
+++ KT-SAMS/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("js") version "1.4-M1"
+}
+
+repositories {
+    jcenter()
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+}
+
+kotlin {
+    target {
+        useCommonJs()
+
+        browser()
+        // processExecutable()
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib-js"))
+    testImplementation(kotlin("test-js"))
+}
+
+tasks {
+    wrapper {
+        gradleVersion = "6.3"
+        distributionType = Wrapper.DistributionType.ALL
+    }
+}
---
+++ KT-SAMS/settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "sams"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    }
+}
