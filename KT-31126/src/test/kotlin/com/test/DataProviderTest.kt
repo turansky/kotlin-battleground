359717402bcd9f31b6594f232ff2d472355dc7b8
Victor Turansky <victor.turansky@gmail.com>
2020-03-30 04:25:17 +0300

    KT-31126. Add tests

--- KT-31126/src/test/kotlin/com/test/DataProviderTest.kt
+++ KT-31126/src/test/kotlin/com/test/DataProviderTest.kt
@@ -3,11 +3,26 @@ package com.test
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
+@ExperimentalJsExport
 class DataProviderTest {
     @Test
-    fun primary() {
+    fun externalPrimary() {
         val provider = DataProvider("13")
 
         assertEquals("13", provider.data)
+    }
+
+    @Test
+    fun primaryToPrimary() {
+        val provider = StringDataProvider("42")
+
+        assertEquals("42", provider.data)
+    }
+
+    @Test
+    fun primaryToSecondary() {
+        val provider = DataProvider(777)
+
+        assertEquals(777, provider.data)
     }
 }
