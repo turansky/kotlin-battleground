0b6ccaad4615068625e3a2cfcfefaa7dbe7b022c
Victor Turansky <victor.turansky@gmail.com>
2020-03-30 05:02:51 +0300

    KT-31126. Update annotations

--- KT-31126/src/main/kotlin/com/test/CustomDataProvider.kt
+++ KT-31126/src/main/kotlin/com/test/CustomDataProvider.kt
@@ -1,11 +1,15 @@
 package com.test
 
+@JsExport
+@ExperimentalJsExport
 class StringDataProvider(data: String) : DataProvider(data) {
     fun doString() {
         println("String")
     }
 }
 
+@JsExport
+@ExperimentalJsExport
 class IntDataProvider(data: Int) : DataProvider(data) {
     fun doInt() {
         println("Int")
