54c1e817df9975fa3e863c3dac4ba2ceb0ac4a9f
Victor Turansky <victor.turansky@gmail.com>
2020-04-11 20:53:00 +0300

    KT-SUBP. subprojects in example

--- KT-SUBP/build.gradle.kts
+++ KT-SUBP/build.gradle.kts
@@ -2,49 +2,19 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
 import org.jetbrains.kotlin.gradle.plugin.KotlinJsPluginWrapper
 
 plugins {
-    kotlin("js") version "1.4-M1"
+    kotlin("js") version "1.4-M1" apply false
 }
 
-repositories {
-    jcenter()
-    maven("https://dl.bintray.com/kotlin/kotlin-eap")
-    maven("https://dl.bintray.com/kotlin/kotlin-dev")
-}
-
-kotlin {
-    target {
-        browser()
-    }
-}
-
-dependencies {
-    implementation(kotlin("stdlib-js"))
-}
-
-tasks {
-    named<KotlinJsCompile>("compileKotlinJs") {
-        doFirst {
-            check(kotlinOptions.moduleKind == "commonjs") {
-                """
-                   Expected module kind: commonjs
-                   Actual module kind: ${kotlinOptions.moduleKind}
-               """.trimIndent()
-            }
-
-            println("OK")
-        }
+subprojects {
+    repositories {
+        jcenter()
+        maven("https://dl.bintray.com/kotlin/kotlin-eap")
+        maven("https://dl.bintray.com/kotlin/kotlin-dev")
     }
 
-    wrapper {
-        gradleVersion = "6.3"
-        distributionType = Wrapper.DistributionType.ALL
-    }
-}
-
-allprojects {
     plugins.withType<KotlinJsPluginWrapper> {
         tasks.withType<KotlinJsCompile> {
-            kotlinOptions.moduleKind == "commonjs"
+            kotlinOptions.moduleKind = "commonjs"
         }
     }
 }
--- KT-SUBP/settings.gradle.kts
+++ KT-SUBP/settings.gradle.kts
@@ -7,3 +7,5 @@ pluginManagement {
         maven("https://dl.bintray.com/kotlin/kotlin-dev")
     }
 }
+
+include("lib")
