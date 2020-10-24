b1cb101e9eb6df5f0c0223d014b3ecd076c5f007
Victor Turansky <victor.turansky@gmail.com>
2020-10-25 02:39:53 +0300

    KT-40235 Update test example

--- KT-40235/src/main/kotlin/Object.kt
+++ KT-40235/src/main/kotlin/Object.kt
@@ -6,3 +6,18 @@ external interface Object {
         fun keys(o: Any): Array<out String>
     }
 }
+
+@JsName("Object2")
+external class Object2 {
+    companion object {
+        fun keys(o: Any): Array<out String>
+    }
+}
+
+@JsExport
+@ExperimentalJsExport
+val data: Any = Object.Companion
+
+@JsExport
+@ExperimentalJsExport
+val data2: Any = Object2.Companion
