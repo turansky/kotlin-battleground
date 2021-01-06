b360758da2869a19bef2c063e03d3dc6e0cdf652
Victor Turansky <victor.turansky@gmail.com>
2021-01-06 07:01:49 +0300

    KT-39602 Test example

---
+++ KT-39602/src/main/kotlin/App.kt
@@ -0,0 +1,21 @@
+@JsName("Array")
+external interface IArray {
+    val length: Int
+
+    @Suppress("NESTED_CLASS_IN_EXTERNAL_INTERFACE")
+    companion object {
+        fun of(item: Any): IArray
+
+        fun of(vararg items: Any): IArray
+    }
+}
+
+@JsExport
+@ExperimentalJsExport
+fun test() {
+    val a = IArray.of(13)
+    println("Length: ${a.length}")
+
+    val b = IArray.of(4, 8, 15, 16, 23, 42)
+    println("Length: ${b.length}")
+}
