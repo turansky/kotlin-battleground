44ac97d2cb44698487c398e72daa2c47f604df48
Victor Turansky <victor.turansky@gmail.com>
2021-02-15 16:40:19 +0300

    KTOR-1084 Add `per-module` flag

--- KTOR-1084/build.gradle.kts
+++ KTOR-1084/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
+
 plugins {
     kotlin("js") version "1.4.30"
 }
@@ -18,6 +20,13 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.4.2")
     implementation("io.ktor:ktor-client-js:master-26")
     implementation(devNpm("webpack-bundle-analyzer", "4.4.0"))
+}
+
+tasks.withType<KotlinJsCompile>().configureEach {
+    kotlinOptions.freeCompilerArgs += listOf(
+        // "-Xir-property-lazy-initialization",
+        "-Xir-per-module"
+    )
 }
 
 tasks {
