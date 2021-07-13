c5fc18e6c24c9b72a30985f3f836e29a8f5ecc82
Victor Turansky <victor.turansky@gmail.com>
2021-07-13 13:23:24 +0300

    KTOR-1084 Simplify `main` declaration

--- KTOR-1084/src/main/kotlin/App.kt
+++ KTOR-1084/src/main/kotlin/App.kt
@@ -1,13 +1,9 @@
 import io.ktor.client.*
 import io.ktor.client.request.*
 import kotlinx.browser.document
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
-fun main() {
-    GlobalScope.launch {
-        val client = HttpClient()
-        val resp = client.get<String>("https://httpbin.org/get")
-        document.write(resp)
-    }
+suspend fun main() {
+    val client = HttpClient()
+    val resp = client.get<String>("https://httpbin.org/get")
+    document.write(resp)
 }
